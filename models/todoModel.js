a9e65c8207acaf4a80cd10d8d0aba97162a5c3af
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-07-08 16:33:16 -0400

    created to do model

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,1 +1,16 @@
-// export const Todo = mongoose.model('Todo', { name: String });
+import mongoose from 'mongoose';
+
+const todoSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        completed: {
+            type: Boolean,
+            required: true
+        }
+    }
+);
+
+export const Todo = mongoose.model('Todo', { name: String });
